bc5929f4475ac3421cb11600c4d972d557362c28
Pra5hant <prasshant2580thakur@gmail.com>
2025-04-30 11:46:15 +0545

    Added more logging and test route to fix 404

--- server.js
+++ server.js
@@ -209,5 +209,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
