585892006cc2a4584bedf8286f9a8cc937facd18
Pra5hant <prasshant2580thakur@gmail.com>
2025-04-30 11:38:42 +0545

    Added SSL to PostgreSQL connection

--- server.js
+++ server.js
@@ -25,7 +25,11 @@ const pool = new Pool({
   host: process.env.host,
   database: process.env.database,
   password: process.env.password,
-  port: process.env.port
+  port: process.env.port,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect((err) => {
